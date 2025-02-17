e7dc2544161a6bc6a8151d15bff25aa8cd531141
Jorge Alves <jorge.mnf.alves@gmail.com>
2025-02-17 14:05:23 +0100

    none

--- app/mc.php
+++ app/mc.php
@@ -1,3 +1,23 @@
 <?php
-    $PDO = new PDO("mysql:host=".$_ENV['DB_HOST'].";dbname=".$_ENV['DB_NAME'],$_ENV['DB_USER'],$_ENV['DB_PASSWORD']);
+    try 
+    {
+        $conn = new PDO("mysql:host=" . $_ENV['DB_HOST'] . ";dbname=" . $_ENV['DB_NAME'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD'] );
+        
+        // Set PDO attributes for error handling
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+        $stmt = $conn->prepare("SELECT * FROM produit");
+        $stmt->execute();
+        
+        // Fetch results if needed
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        print "<pre>";
+        print_r($results);  // Optionally display the results
+        print "</pre>";
+        
+    } 
+    catch (PDOException $e) 
+    {
+        echo "Connection failed: " . $e->getMessage();
+    }
 ?>
